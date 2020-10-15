28749b5a7ef5b58ab40333fea66693a341f49050
Wilhelm Duembeg <wilhelm.duembeg@dura-machines.com>
2020-10-14 21:30:07 -0700

    Get server external IP and bind

--- modules/remote_server.py
+++ modules/remote_server.py
@@ -177,7 +177,7 @@ class RemoteServerThread(threading.Thread, gc.EventQueueIf):
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server.setblocking(0)
-            self.server.bind(('', self.port))
+            self.server.bind(("{}.local".format(self.host), self.port))
             self.server.listen(5)
             self.inputs.append(self.server)
 
